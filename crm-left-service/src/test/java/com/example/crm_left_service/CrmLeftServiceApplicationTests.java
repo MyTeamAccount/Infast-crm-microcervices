e5ff93288f28be7a5e9ff2a3a916c02355fbc422
MyTeamAccount <sabinasayfullayeva292@gmail.com>
2024-11-25 15:32:48 +0300

    Initial commit

---
+++ crm-left-service/src/test/java/com/example/crm_left_service/CrmLeftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crm_left_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmLeftServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ messenger-service/src/main/java/com/example/messenger_service/MessengerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.messenger_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MessengerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerServiceApplication.class, args);
+	}
+
+}
