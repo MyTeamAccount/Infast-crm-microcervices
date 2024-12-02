453bcee55b64780d383cc306784b72a8bc56a478
MyTeamAccount <sabinasayfullayeva292@gmail.com>
2024-12-02 09:21:26 +0300

    for checkout

--- crm-left-service/src/main/java/com/example/crm_left_service/client/UserServiceClient.java
+++ crm-left-service/src/main/java/com/example/crm_left_service/client/UserServiceClient.java
@@ -1,6 +1,8 @@
 package com.example.crm_left_service.client;
 
+import com.example.crm_left_service.payload.user.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
